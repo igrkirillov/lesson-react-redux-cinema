d20b2d9867e62699db47986e0ab44e54134466cf
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-08-21 15:48:45 +0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import {StrictMode} from 'react'
-import {createRoot} from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import {HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <HashRouter basename={import.meta.env.BASE_URL}>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <App />
-      </HashRouter>
+      </BrowserRouter>
   </StrictMode>,
 )
